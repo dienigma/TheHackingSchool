6d102c0f0740755834d070bbc9588ce7884450e5
dienigma <joshichinmay09@gmail.com>
2019-03-23 00:26:41 +0530

    Solved the first problem in section 2 using JS. The code uses a single if elese statement.

---
+++ Assignments/Week_01/evenOddJs.js
@@ -0,0 +1,6 @@
+const checkOddEven = (number) => number%2 === 0 ? console.log("number is even") : console.log("number is odd");
+
+checkOddEven(2);
+checkOddEven(3);
+checkOddEven(5);
+checkOddEven(7);
