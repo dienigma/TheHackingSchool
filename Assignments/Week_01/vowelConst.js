f01ca93d78d63dd26a474734c5b418f89695398e
dienigma <joshichinmay09@gmail.com>
2019-03-24 14:50:12 +0530

    Solution to question 10 in JavaScript

---
+++ Assignments/Week_01/vowelConst.js
@@ -0,0 +1,4 @@
+const vowelConst = (alphabet) => alphabet === 'a' || alphabet === 'i' || alphabet === 'o' || alphabet === 'e' || alphabet === 'u' ? console.log("The character is a vowel") : console.log("The character is a consonant")
+
+vowelConst("a")
+vowelConst("x")
