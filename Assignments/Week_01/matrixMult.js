c8effcc253ca6a6784c8f9a737c4a8257d9e1438
dienigma <joshi.chinmay09@gmail.com>
2019-03-26 11:54:37 +0530

    Solved matrix multiplication. Question 20 in Js.

--- Assignments/Week_01/matrixMult.js
+++ Assignments/Week_01/matrixMult.js
@@ -1,3 +1,20 @@
-const multiply = (x,y) => {
-	
+function multiply(a, b) {
+  var aNumRows = a.length, aNumCols = a[0].length,
+      bNumRows = b.length, bNumCols = b[0].length,
+      m = new Array(aNumRows);  // initialize array of rows
+  for (var r = 0; r < aNumRows; ++r) {
+    m[r] = new Array(bNumCols); // initialize the current row
+    for (var c = 0; c < bNumCols; ++c) {
+      m[r][c] = 0;             // initialize the current cell
+      for (var i = 0; i < aNumCols; ++i) {
+        m[r][c] += a[r][i] * b[i][c];
+      }
+    }
+  }
+  return m;
 }
+
+var a = [[8, 3], [2, 4], [3, 6]],
+    b = [[1, 2, 3], [4, 6, 8]];
+
+console.log(multiply(a,b))
