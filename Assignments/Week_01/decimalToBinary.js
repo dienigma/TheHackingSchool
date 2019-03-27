7cdedaf3218619549c48dd9813a08ede0795d608
dienigma <joshi.chinmay09@gmail.com>
2019-03-27 18:34:10 +0530

    Solved question 25 in JS

---
+++ Assignments/Week_01/decimalToBinary.js
@@ -0,0 +1,11 @@
+const convertToBinary = (decimal) => {
+	let result = []
+	if (decimal == 0){
+		return;
+	} else {
+		console.log(decimal % 2)
+		return convertToBinary(parseInt(decimal / 2))
+	}
+}
+
+convertToBinary(25)
