8a6245d0d8c3d85d0d229f1dd63ea640c9113744
dienigma <joshi.chinmay09@gmail.com>
2019-03-29 14:44:05 +0530

    Fixed bugs and also solved the second part of the problem

--- Assignments/Week_01/generateOddEven.js
+++ Assignments/Week_01/generateOddEven.js
@@ -10,5 +10,26 @@ const genEven = (n) => {
 	}
 }
 
-genEven(7);
-genOdd(12);
+// genEven(7);
+// genOdd(12);
+
+
+const genOddRange = (n) => {
+    curr = 1
+    for( var i = 0; i < n; i++){
+        console.log(curr)
+        curr += 2
+    }
+}
+
+genOddRange(9)
+
+const genEvenRange = (n) => {
+    curr = 0
+    for (var i = 0; i <n; i++){
+        console.log(curr)
+        curr +=2
+    }
+}
+
+genEvenRange(9)
