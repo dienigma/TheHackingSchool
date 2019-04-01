02ffd35b3b88b44e332ca2d19ee458cbcc71985d
dienigma <joshi.chinmay09@gmail.com>
2019-04-01 10:52:35 +0530

    Fixed bugs with fizzBuzz

--- Assignments/Week_03/fizzBuzz.js
+++ Assignments/Week_03/fizzBuzz.js
@@ -1,10 +1,12 @@
 const fizzBuzz = (n) => {
-    for (var i = 0; i < n; i++){
+    for (var i = 0; i < n+1; i++){
         if (i % 3 === 0){
             console.log("Fizz")
         }
         if (i % 5 === 0){
             console.log("Buzz")
+        }else {
+            console.log(i)
         }
     }
 }
--- Assignments/Week_03/fizzBuzz.py
+++ Assignments/Week_03/fizzBuzz.py
@@ -1,8 +1,10 @@
 def fizzBuzz (n):
-    for i in range(0,n):
+    for i in range(n+1):
         if (i % 3 == 0):
             print("fizz")
         if (i % 5 == 0):
             print("Buzz")
+        else: 
+            print (i)
 
 fizzBuzz(100)
