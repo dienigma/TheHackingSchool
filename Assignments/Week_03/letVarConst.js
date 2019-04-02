dca0952cc2a0de0fc83f5caacd613577f5cb12ca
dienigma <joshi.chinmay09@gmail.com>
2019-04-02 16:36:19 +0530

    Difference between let, var and const

---
+++ Assignments/Week_03/letVarConst.js
@@ -0,0 +1,14 @@
+for (var  i  =  0; i  <  5;  i++){
+    console.log(i)
+}
+
+console.log(i)
+// prints 5
+
+
+for (let j = 0; j < 5; j++){
+    console.log(j)
+}
+
+console.log(j)
+//  ReferenceError: j is not defined
