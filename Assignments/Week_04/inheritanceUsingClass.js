964e8a9d8d83e43368c8794058f48d4000ea4d2e
dienigma <joshi.chinmay09@gmail.com>
2019-04-08 22:46:52 +0530

    Buggy

---
+++ Assignments/Week_04/inheritanceUsingClass.js
@@ -0,0 +1,44 @@
+class Character{
+    constructor(name,level,guild){
+        this.name = name
+        this.level = level
+        this.guild = guild
+    }
+
+    bio(){
+        return {
+            name: this.name,
+            level: this.level,
+            guild: this.guild
+        }
+    }
+}
+
+// var dienigma = new Character("Chinmay",15,"Crimson Knights")
+
+// console.log(dienigma.bio())
+
+class Assassin extends Character{
+    constructor(name,level,guild,skills, dps){
+        super()
+        this.skills = skills
+        this.dps = dps
+    }
+    profile(){
+        return {
+            skills: this.skills,
+            dps: this.dps
+        }
+    }
+    stats(){
+        return{
+            bio: this.bio(),
+            profile: this.profile()
+            
+        }
+    }
+}
+
+var dienigma = new Assassin("Chinmay",15,"Crimson Knights",["charge","blink","phantom dagger","juxtapose"],59)
+
+console.log(dienigma.stats())
