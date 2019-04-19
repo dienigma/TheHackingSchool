d9d4c6bf247befcd995a56f5f28b429940ac892f
dienigma <joshi.chinmay09@gmail.com>
2019-04-20 01:38:33 +0530

    Built async bcryptjs app. This solved the assignment

--- Assignments/Week_05/bycrypt-async/routes/index.js
+++ Assignments/Week_05/bycrypt-async/routes/index.js
@@ -1,9 +1,22 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-
+const bcrypt = require("bcryptjs");
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function(req, res, next) {
+  res.render("crypter");
+});
+
+router.post("/", function(req, res, next) {
+  bcrypt.genSalt(10, function(err, salt) {
+    bcrypt.hash(req.body.ipstring, salt, function(err, hash) {
+      // Store hash in your password DB.
+      if (err) {
+        res.send(err);
+      } else {
+        res.json(hash);
+      }
+    });
+  });
 });
 
 module.exports = router;
